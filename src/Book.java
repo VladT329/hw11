771f9a39e8b422836a1ca7da96ee8778ffaed6a2
Vladislav <VladT329@gmail.com>
2024-05-27 23:41:57 +0300

    start coding

---
+++ src/Book.java
@@ -0,0 +1,11 @@
+public class Book {
+    String name;
+    Author author;
+    int year;
+
+    public Book (String name, Author author, int year){
+        this.name = name;
+        this.author = author;
+        this.year = year;
+    }
+}
