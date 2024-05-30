567e3a052e7136878eae62d869456a62dc58c73f
Vladislav <VladT329@gmail.com>
2024-05-30 23:55:25 +0300

    done

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,15 @@ public class Main {
         System.out.println("idot.getYear() = " + idiot.getYear());
         System.out.println("idiot.toString() = " + idiot.toString());
         System.out.println("masterImargarita.toString() = " + masterImargarita.toString());
+        System.out.println();
+        System.out.println("Сравнение книг через hashCode");
+        System.out.println("idiot.hashCode() = " + idiot.hashCode());
+        System.out.println("masterImargarita.hashCode() = " + masterImargarita.hashCode());
+        System.out.println(idiot.equals(masterImargarita));
+        System.out.println();
+        System.out.println("Сравнение авторов через hashCode");
+        System.out.println("bulgakov.hashCode() = " + idiot.hashCode());
+        System.out.println("dostoevskiy.hashCode() = " + idiot.hashCode());
+        System.out.println("bulgakov.equals(dostoevskiy) = " + bulgakov.equals(dostoevskiy));
     }
 }
